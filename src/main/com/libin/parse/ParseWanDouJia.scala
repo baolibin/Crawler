6934238b9d2449c180ab54fa71b55b9c852fc949
baolibin <baolibin@xiaomi.com>
2017-10-16 20:27:52 +0800

    绝对路径改为相对路径

--- src/main/com/libin/parse/ParseWanDouJia.scala
+++ src/main/com/libin/parse/ParseWanDouJia.scala
@@ -3,8 +3,6 @@ package libin.parse
 import libin.utils.HtmlUtils
 import org.htmlcleaner.{TagNode, HtmlCleaner}
 
-import scala.StringBuilder
-
 /**
   * Created by baolibin on 2017/10/15.
   */
@@ -16,10 +14,32 @@ object ParseWanDouJia {
     val sb: StringBuilder = new StringBuilder
     val htmlCleaner: HtmlCleaner = new HtmlCleaner
     val rootNode: TagNode = htmlCleaner.clean(content)
-    val level1CategoryName = HtmlUtils.getText(rootNode, "/html/body/div[3]/div[1]/div[1]/a/span")
-    println("一级标题:" + level1CategoryName)
-    val level2CategoryName = HtmlUtils.getText(rootNode, "/html/body/div[3]/div[1]/div[2]/a/span")
-    println("二级标题:" + level2CategoryName)
+    val level1CategoryName = HtmlUtils.getText(rootNode, "//div[@class=\"container\"]/div[1]/div[1]/a/span")
+    println("一级标题:" + level1CategoryName.trim)
+    val level2CategoryName = HtmlUtils.getText(rootNode, "//div[@class=\"container\"]/div[1]/div[2]/a/span")
+    println("二级标题:" + level2CategoryName.trim)
+    val appName = HtmlUtils.getText(rootNode, "//div[@class=\"container\"]/div[2]/div[1]/div[2]/p[1]/span")
+    println("App名字:" + appName.trim)
+    val downloads = HtmlUtils.getText(rootNode, "//div[@class=\"container\"]/div[2]/div[1]/div[4]/span[1]/i")
+    println("下载量:" + downloads.trim)
+    val favorableRate = HtmlUtils.getText(rootNode, "//div[@class=\"container\"]/div[2]/div[1]/div[4]/span[2]/i")
+    println("好评率:" + favorableRate.trim)
+    val commentNumber = HtmlUtils.getText(rootNode, "//div[@class=\"container\"]/div[2]/div[1]/div[4]/div[1]/a/i")
+    println("评论数:" + commentNumber.trim)
+    val comment1 = HtmlUtils.getText(rootNode, "//div[@class=\"container\"]/div[2]/div[1]/div[2]/p[2]")
+    println("小编或用户点评1:" + comment1.trim)
+    val comment2 = HtmlUtils.getText(rootNode, "//div[@class=\"container\"]/div[2]/div[2]/div[1]/div[1]/div")
+    println("小编或用户点评2:" + comment2.trim)
+    val size = HtmlUtils.getText(rootNode, "//div[@class=\"container\"]/div[2]/div[2]/div[2]/div[1]/dl/dd[1]")
+    println("大小:" + size.trim)
+    val updateTime = HtmlUtils.getText(rootNode, "//*[@id=\"baidu_time\"]")
+    println("更新时间:" + updateTime.trim)
+    val versionName = HtmlUtils.getText(rootNode, "//div[@class=\"container\"]/div[2]/div[2]/div[2]/div[1]/dl/dd[5]")
+    println("版本:" + versionName.trim.split(";")(1))
+    val compatibility = HtmlUtils.getText(rootNode, "//div[@class=\"container\"]/div[2]/div[2]/div[2]/div[1]/dl/dd[6]")
+    println("兼容性:" + compatibility.trim.split("\n")(0))
+    val publisherName = HtmlUtils.getText(rootNode, "//div[@class=\"container\"]/div[2]/div[2]/div[2]/div[1]/dl/dd[7]/span")
+    println("开发商|作者:" + publisherName.trim)
     sb.toString()
   }
 }
--- src/main/com/libin/startCrawl/WanDouJia.scala
+++ src/main/com/libin/startCrawl/WanDouJia.scala
@@ -2,8 +2,6 @@ package libin.startCrawl
 
 import libin.download.DownloadInfo
 import libin.parse.ParseWanDouJia
-import libin.utils.HtmlUtils
-import org.htmlcleaner.{TagNode, HtmlCleaner}
 
 /**
   * Created by baolibin on 2017/10/15.
@@ -19,12 +17,10 @@ object WanDouJia {
   def main(args: Array[String]) {
     //下载豌豆荚页面
     val wanDouJiaPage: String = DownloadInfo.downloadWanDouJia(url)
-    println(wanDouJiaPage)
+    //println(wanDouJiaPage)
     //解析豌豆荚页面
-    val htmlCleaner: HtmlCleaner = new HtmlCleaner
-    val rootNode: TagNode = htmlCleaner.clean(wanDouJiaPage)
-    println("为什么是空：" + HtmlUtils.getText(rootNode, "/html/body[@class='detail PC ']/div[@class='container']/div[@class='detail-wrap ']/div[@class='detail-top clearfix']/div[@class='app-info']/p[@class='app-name']/span[@class='title']"))
-    //ParseWanDouJia.parseWanDouJia(wanDouJiaPage)
+    ParseWanDouJia.parseWanDouJia(wanDouJiaPage)
+
   }
 
 }
